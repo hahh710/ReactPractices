c067ea3c202e560828a909b3c4f25554cf6b742c
Hun <hunhoha@cmail.carleton.ca>
2020-01-04 18:34:53 -0500

    Making API Request
    1. Learned What is AJAX and What it does
      - Used AXIOS Package
    2. Asyncronous Method
      - Promise .then(()=>({}))
    3. Seperating AXIOS Configuration file
     - contains Keys

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,13 +1,53 @@
 import React from "react";
+import unsplash from "../api/unsplash";
 import SearchBar from "./SearchBar";
 
-const App = () => {
+class App extends React.Component {
+  state = {
+    images: []
+  };
 
+  // Async 를 사용함으로써 async, await 이라는 사용할수있고
+  //await를 사용함으로써 Then() promise를 다른식으로 할수있게 해줄수있다.
+  onSearchSubmit = async term => {
+    // to fix direction of this,
 
-  return (
-    <div className="ui container" style={{ marginTop: "10px" }}>
-      <SearchBar></SearchBar>
-    </div>
-  );
-};
+    //axios.get() function takes 2 arguments
+    //1. the address that we want to make a request to
+    //2. the object that contains the options to customize the request
+    const asyncsResponse = await unsplash
+      .get("/search/photos", {
+        params: { query: term }
+        // params will end up "https://api.unsplash.com/search/photos? query=term"
+
+        /*  This is written in api configuration file.
+        headers: {
+          Authorization:
+            "Client-ID be67707be4cee67eb7af05f6bac04fb22b42b697b6632eb60a05cba8473065ab"
+        }
+        */
+      })
+      .then(response => {
+        //.then() is called "promise" which method or object that works after the completion of the Asyncronous method.
+        console.log(response.data.results);
+
+        // writing this will cause an error, to fix there are 3 ways to do it
+        //1. binding it 2. writing arrow function inside of <SearchBar onSubmit={()=>}>
+        this.setState({
+          images: response.data.results
+        });
+      });
+    //this is same as response
+    //console.log(asyncsResponse);
+  };
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit}></SearchBar>
+        Found: {this.state.images.length} images
+      </div>
+    );
+  }
+}
 export default App;
--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,18 +1,32 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-  //event Object is normal Javascript Event that contains information about what just occurs
+  state = { term: "" };
+
+  /*
+    //event Object is normal Javascript Event that contains information about what just occurs
   onInputChange(event) {
     console.log(event.target.value); //event.target.value- this contains the value of what user just inputed.
+  }*/
+
+  onFormSubmit(event) {
+    event.preventDefault(); // preventing disappearing of text when page is refreshed
+    // DON"T UNDERSTAND...
+    this.props.onSubmit(this.state.term);
   }
 
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={event => this.onFormSubmit(event)} className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text" onChange={this.onInputChange} />
+            <input
+              type="text"
+              value={this.state.term} //Input already knows the value, but we overrides from state
+              onChange={e => this.setState({ term: e.target.value })}
+              // by doing this, we save the value into the state not to grab the data from DOM
+            />
           </div>
         </form>
       </div>
